f15b886b60efdaa06ccd85c02f6c1167b279ea1c
jason <642405729@qq.com>
2020-08-12 19:32:06 +0800

    2020-08-12 19:32:05

--- database/seeds/GoodAttrNamesSeeder.php
+++ database/seeds/GoodAttrNamesSeeder.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\GoodAttrName;
+use Carbon\Carbon;
 
 class GoodAttrNamesSeeder extends Seeder
 {
+    /**
+     * 待批量插入数据
+     *
+     * @var array
+     */
+    private $data = [];
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +20,29 @@ class GoodAttrNamesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->formatData();
+
+        GoodAttrName::query()->insert($this->data);
+    }
+
+    /**
+     * 格式化数据
+     */
+    private function formatData()
+    {
+        $this->data[] = [
+            'id' => 1,
+            'good_id' => 1,
+            'name' => '颜色',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
+        ];
+        $this->data[] = [
+            'id' => 2,
+            'good_id' => 1,
+            'name' => '尺码',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
+        ];
     }
 }
